9ccbd2763dc9d5b3b62375abe74f12b752fbbf33
patcharanut-bit <patcharanut.nut@hotmail.com>
2021-01-17 11:02:47 +0700

    update joinset.py

--- Set/joinset.py
+++ Set/joinset.py
@@ -12,7 +12,14 @@ set1.update(set2)
 print(set1) # {'d', 'e', 'c', 'a', 'f', 'b'}
 
 # EX3
-set1 = {"a", "b", "c"}
-set2 = {"d", "e", "f"}
-set1.update(set2)
-print(set1) # {'d', 'e', 'c', 'a', 'f', 'b'}
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.intersection_update(set2)
+print(set1) # {'apple'}
+
+# EX4
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.symmetric_difference_update(set2)
+print(set1) # {'banana', 'cherry', 'microsoft', 'google'}
+
